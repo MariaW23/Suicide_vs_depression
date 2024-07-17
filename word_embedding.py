f7cdb2776f3e54b139cee33567361b9aeb6a3ae0
MariaW23 <maria_wang@brown.edu>
2024-07-16 17:40:08 -0700

    train test split created

---
+++ word_embedding.py
@@ -0,0 +1,10 @@
+# Create train test split
+# from sklearn.model_selection import train_test_split
+# import pandas as pd
+
+# data = pd.read_csv('data/suicide_vs_depression.csv')
+# # random state set to random int to make sure randomness is reproducible
+# train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+# train_data.to_csv('data/train_data.csv', index=False)
+# test_data.to_csv('data/test_data.csv', index=False)
+
